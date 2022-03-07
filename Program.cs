902f20e377fd846ada1c535899d073456699ce42
Leon Glass <lg@glasys.co.uk>
2022-03-07 09:23:17 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
